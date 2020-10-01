1237a36d68b301cd215a4fe0b15b66c8c0c32777
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-10-01 19:27:36 +0300

    design

--- app/src/main/java/com/example/matala_083/MainActivity.java
+++ app/src/main/java/com/example/matala_083/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tv1,tv2;
     Spinner spin1,spin2;
     ListView lv;
+    int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
