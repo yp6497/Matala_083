fc25e550aab904db98a16defd24780aea3c44aae
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-10-01 19:26:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/matala_083/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.matala_083;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv1,tv2;
+    Spinner spin1,spin2;
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1=findViewById(R.id.tv1);
+        tv2=findViewById(R.id.tv2);
+        spin1=findViewById(R.id.spin1);
+        spin2=findViewById(R.id.spin2);
+        lv=findViewById(R.id.lv);
+
+    }
+}
