abdf86dcb02582b8c48bdd83fd55b3f36751559a
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-10-15 00:25:47 +0300

    basic code

--- app/src/main/java/com/example/matala_083/MainActivity.java
+++ app/src/main/java/com/example/matala_083/MainActivity.java
@@ -13,80 +13,96 @@ import android.widget.TextView;
 //public class MainActivity extends AppCompatActivity {
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
 
-    TextView tv1,tv2;
-    Spinner spinI,spinM;
+    TextView tv1, tv2;
+    Spinner spinI, spinM;
     ListView lv;
-    int x,pos,y;
-    String [][] Medinot={{"Congo","madagascar","morocco","nigeria","Tanzania","Uganda","sudan"}, {"Argentina", "canada","Brazil","colombia","Mexico","United States","Venezuela"},
-            {"Chaina", "India","Israel","Japan","Russia","Turkey","Yemen"}, {"Bulgaria", "France","Germany","Hungary","Italy","Norway","Sweden"}};
-    String [] Ibashot={"Choose a continent","Africa","America","Asia","Europe"};
-    String [][] CitiesAfrica={{"a","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""}/*1*/,{"b","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},/*2*/
-    {"c","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""}/*3*/,{"d","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""}/*4*/,
-    {"e","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""}/*5*/,{"f","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""}/*6*/,
-    {"f","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""}};/*7*/
-    String [][] arrayS=new String[7][4];
-
+    int x, pos=-1, y,z=-1;
+    String[][] Medinot = {{"Choose a c in af","Congo", "madagascar", "morocco", "nigeria", "Tanzania", "Uganda", "sudan"}, {"Choose a c in am","Argentina", "canada", "Brazil", "colombia", "Mexico", "United States", "Venezuela"},
+            {"Choose a c in as","Chaina", "India", "Israel", "Japan", "Russia", "Turkey", "Yemen"}, {"Choose a c in er","Bulgaria", "France", "Germany", "Hungary", "Italy", "Norway", "Sweden"}};
+    String[] Ibashot = {"Choose a continent", "Africa", "America", "Asia", "Europe"};
+    String [][] CitiesAfrica = {{"a", "", "", "", ""}, {"b", "", "", "", ""}, {"c", "", "", "", ""}, {"d", "", "", "", ""}, {"e", "", "", "", ""}, {"f", "", "", "", ""}, {"g", "", "", "", ""}};
+    String[][] arrayS = new String[7][4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv1=findViewById(R.id.tv1);
-        tv2=findViewById(R.id.tv2);
-        spinI=findViewById(R.id.spin1);
-        spinM=findViewById(R.id.spin2);
-        lv=findViewById(R.id.lv);
-
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        spinI = findViewById(R.id.spin1);
+        spinM = findViewById(R.id.spin2);
+        lv = findViewById(R.id.lv);
 
 
         spinI.setOnItemSelectedListener(this);
         ArrayAdapter<String> adp = new ArrayAdapter<String>(this,
-                R.layout.support_simple_spinner_dropdown_item,Ibashot);
+                R.layout.support_simple_spinner_dropdown_item, Ibashot);
         spinI.setAdapter(adp);
-        //spinI.setOnItemSelectedListener(new IbashitClass());
-        spinM.setOnItemSelectedListener(new MedinotClass());
 
-        lv.setOnItemClickListener(this);
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        //spinI.setOnItemSelectedListener(new IbashitClass());
+        //spinM.setOnItemSelectedListener(new MedinotClass());
+
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if(position==0) {
-            y++;
+        Spinner spinner = (Spinner) parent;
+
+        x=position;
+        pos=position-1;
+
+        if (spinner.getId()==R.id.spin1) {
+            tv2.setText(position+"pos");
+            //lv.setAdapter(null);
+            if (position == 0) {
+                y++;
+            } else {
+                //pos = position - 1;
+                spinM.setOnItemSelectedListener(this);
+                ArrayAdapter<String> a = new ArrayAdapter<String>(this,
+                        R.layout.support_simple_spinner_dropdown_item, Medinot[pos]);
+                spinM.setAdapter(a);
+            }
+        }
+        else if(spinner.getId()== R.id.spin2){
+            //if(position==0) pos=0;
+            //lv.setAdapter(null);
+            if (position == 0) {
+                lv.setAdapter(null);
+            }
+            //if(z==0) lv.setAdapter(null);
+            //else {
+            else {
+                lv.setOnItemClickListener(this);
+                ArrayAdapter<String> adpl = new ArrayAdapter<String>(this,
+                        R.layout.support_simple_spinner_dropdown_item, CitiesAfrica[pos]);
+                lv.setAdapter(adpl);
+            }
+            }
         }
 
-        else {
-            pos = position - 1;
-            spinM.setOnItemSelectedListener(this);
-            ArrayAdapter<String> a = new ArrayAdapter<String>(this,
-                    R.layout.support_simple_spinner_dropdown_item, Medinot[pos]);
-            spinM.setAdapter(a);
-        }
-
-    }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
-
-    class MedinotClass implements AdapterView.OnItemSelectedListener
+    /*lass MedinotClass implements AdapterView.OnItemSelectedListener
     {
         public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-            lv.setOnItemSelectedListener(this);
-            ArrayAdapter<String> ad = new ArrayAdapter<String>(this,
-                    R.layout.support_simple_spinner_dropdown_item,CitiesAfrica);
-            lv.setAdapter(ad);
+        ArrayAdapter<String> adpl = new ArrayAdapter<String>(this,
+                    R.layout.support_simple_spinner_dropdown_item, CitiesAfrica[0]);
+            lv.setAdapter(adpl);
         }
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
         }
     }
-
-
+     */
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
