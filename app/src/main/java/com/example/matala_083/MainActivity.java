746f10929aef241c1b6410369823b6e27437cf4a
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-10-16 00:37:24 +0300

    basic code

--- app/src/main/java/com/example/matala_083/MainActivity.java
+++ app/src/main/java/com/example/matala_083/MainActivity.java
@@ -10,18 +10,23 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-//public class MainActivity extends AppCompatActivity {
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
 
     TextView tv1, tv2;
     Spinner spinI, spinM;
     ListView lv;
-    int x, pos=-1, y,z=-1;
+    int x, pos=-1,pos2=0, y,z,a;
     String[][] Medinot = {{"Choose a c in af","Congo", "madagascar", "morocco", "nigeria", "Tanzania", "Uganda", "sudan"}, {"Choose a c in am","Argentina", "canada", "Brazil", "colombia", "Mexico", "United States", "Venezuela"},
             {"Choose a c in as","Chaina", "India", "Israel", "Japan", "Russia", "Turkey", "Yemen"}, {"Choose a c in er","Bulgaria", "France", "Germany", "Hungary", "Italy", "Norway", "Sweden"}};
     String[] Ibashot = {"Choose a continent", "Africa", "America", "Asia", "Europe"};
     String [][] CitiesAfrica = {{"a", "", "", "", ""}, {"b", "", "", "", ""}, {"c", "", "", "", ""}, {"d", "", "", "", ""}, {"e", "", "", "", ""}, {"f", "", "", "", ""}, {"g", "", "", "", ""}};
-    String[][] arrayS = new String[7][4];
+    String [][] CitiesAmerica = {{"aa", "", "", "", ""}, {"bb", "", "", "", ""}, {"cc", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
+    String [][] CitiesAsia = {{"aaa", "", "", "", ""}, {"bbb", "", "", "", ""}, {"ccc", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
+    String [][] CitiesEurope= {{"aaaa", "", "", "", ""}, {"bbbb", "bbbb", "","", ""}, {"cccc", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
+    String [][] IaboutCitiesAf={{"y","y"},{"n",""},{"",""},{"",""},{"",""}/*1*/,{"z","n"},{"d",""},{"",""},{"",""},{"",""},{"fg",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},
+            {"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"n","n"},{"",""},{"",""},{"",""},{"",""},};
+    String [][] arrayS = new String[7][5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,18 +55,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Spinner spinner = (Spinner) parent;
 
-        x=position;
+        Spinner spinner = (Spinner) parent;
         pos=position-1;
 
         if (spinner.getId()==R.id.spin1) {
+            if(position==1) arrayS=CitiesAfrica;
+            else if (position==2) arrayS=CitiesAmerica;
+            else if (position==3) arrayS=CitiesAsia;
+            else if(position==4) arrayS=CitiesEurope;
             tv2.setText(position+"pos");
-            //lv.setAdapter(null);
             if (position == 0) {
                 y++;
             } else {
-                //pos = position - 1;
                 spinM.setOnItemSelectedListener(this);
                 ArrayAdapter<String> a = new ArrayAdapter<String>(this,
                         R.layout.support_simple_spinner_dropdown_item, Medinot[pos]);
@@ -69,18 +75,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         }
         else if(spinner.getId()== R.id.spin2){
-            //if(position==0) pos=0;
-            //lv.setAdapter(null);
+            z=position-1;
             if (position == 0) {
                 lv.setAdapter(null);
             }
-            //if(z==0) lv.setAdapter(null);
-            //else {
             else {
                 lv.setOnItemClickListener(this);
                 ArrayAdapter<String> adpl = new ArrayAdapter<String>(this,
-                        R.layout.support_simple_spinner_dropdown_item, CitiesAfrica[pos]);
+                        R.layout.support_simple_spinner_dropdown_item, arrayS[z]);
                 lv.setAdapter(adpl);
+                a=pos;
+                tv1.setText(a+"a");
             }
             }
         }
@@ -107,6 +112,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+        /*if(a>0) {
+            for (int i = 0; i < a; i++) {
+                pos2 = pos2 + 5;
+            }
+        }
+         */
+        if(a==1) x=5;
+        else if(a==2) x=10;
+        else if(a==3) x=15;
+        else if(a==4) x=20;
+        else if(a==5) x=25;
+        else if(a==6) x=30;
+        x=x+position;
+        //else if(a==0) pos2=position;
+        //if(position==0) pos2=position;
+        //else if(position>0) pos2=position+5;
+        tv1.setText(IaboutCitiesAf[x][0]+" p");
+        tv2.setText(IaboutCitiesAf[x][1]+" po");
     }
 
 
