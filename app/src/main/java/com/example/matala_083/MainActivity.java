f726d4809bb0e60caa238737102cb2b80f511007
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-10-16 12:30:02 +0300

    basic code

--- app/src/main/java/com/example/matala_083/MainActivity.java
+++ app/src/main/java/com/example/matala_083/MainActivity.java
@@ -20,12 +20,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     String[][] Medinot = {{"Choose a c in af","Congo", "madagascar", "morocco", "nigeria", "Tanzania", "Uganda", "sudan"}, {"Choose a c in am","Argentina", "canada", "Brazil", "colombia", "Mexico", "United States", "Venezuela"},
             {"Choose a c in as","Chaina", "India", "Israel", "Japan", "Russia", "Turkey", "Yemen"}, {"Choose a c in er","Bulgaria", "France", "Germany", "Hungary", "Italy", "Norway", "Sweden"}};
     String[] Ibashot = {"Choose a continent", "Africa", "America", "Asia", "Europe"};
-    String [][] CitiesAfrica = {{"a", "", "", "", ""}, {"b", "", "", "", ""}, {"c", "", "", "", ""}, {"d", "", "", "", ""}, {"e", "", "", "", ""}, {"f", "", "", "", ""}, {"g", "", "", "", ""}};
-    String [][] CitiesAmerica = {{"aa", "", "", "", ""}, {"bb", "", "", "", ""}, {"cc", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
-    String [][] CitiesAsia = {{"aaa", "", "", "", ""}, {"bbb", "", "", "", ""}, {"ccc", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
-    String [][] CitiesEurope= {{"aaaa", "", "", "", ""}, {"bbbb", "bbbb", "","", ""}, {"cccc", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
-    String [][] IaboutCitiesAf={{"y","y"},{"n",""},{"",""},{"",""},{"",""}/*1*/,{"z","n"},{"d",""},{"",""},{"",""},{"",""},{"fg",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},
-            {"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"",""},{"n","n"},{"",""},{"",""},{"",""},{"",""},};
+    String [][] CitiesAfrica = {{"Bandundu", "Boma", "Bukavu", "Kananga", "Lubumbashi"}, {"Antananarivo","Antsirabe", "Antsiranana", "Toamasina","Toliara"}, {"Azrou","Berrechid","Casablanca", "Tangier","Khenifra"}, {"Aba", "Bida", "Ikare", "Kano", "Lagos"},
+            {"Dodoma", "Handeni","Iringa", "Lindi", "Mpanda"}, {"Entebbe", "Hoima", "Gulu", "Masaka", "Ssabagabo"}, {"Al-Fashir", "El-Obied", "Nyala", "Omdurman", "Port Sudan"}};
+    String [][] CitiesAmerica = {{"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
+    String [][] CitiesAsia = {{"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
+    String [][] CitiesEurope = {{"", "", "", "", ""}, {"", "", "","", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}, {"", "", "", "", ""}};
+    String [][] IaboutCitiesAf = {{"222 km2","143,435"},{"65 km2","162,521"},{"60 km2 ","870,954"},{"7,428 km2 ","1,271,704"},{"747 km2","1,794,118"}/*1*/,{"85.01 km2","1,275,207"},{"132.94 km2","257,163"},{"42 km2","115,015"},{"20 km2","300,813"},{"16 km2","156,710"},/*2*/
+            {"9 km2","81,350"},{"43 km2","136,634"},{"220 km2","3,359,818"},{"150 km2","947,952"},{"228,567","34 km2"},/*3*/{"72 km2","534,265"},{"51 km2","188,181"},{"41 km2","160,600"},{"449 km2","2,828,861"},{"1,171.3 km2","8,048,430"},/*4*/{"2,576 km2","410,956"},
+            {"8,322 km2","79,056"}, {"458 km2","1,211,900"},{"5,673 km2","41,549"},{"6,655 km2","45,977"},/*5*/{"56.2 km2","69,958"},{"23 km2","100,099"},{"35 km2","152,276"},{"44 km2","103,829"},{"61 km2","284,067"},/*6*/{"73 km2","263,243"},{"58 km2","418,280"},{"55 km2","565,734"},{"614.9 km2","2,395,159"},{"172 km2","489,725"},/*7*/};
+    String [][] IaboutCitiesAm = {{"",""},{"",""},{"",""},{"",""},{"",""},/*1*/{"",""},{"",""},{"",""},{"",""},{"",""},/*2*/
+            {"",""},{"",""},{"",""},{"",""},{"",""},/*3*/{"",""},{"",""},{"",""},{"",""},{"",""},/*4*/
+            {"",""},{"",""},{"",""},{"",""},{"",""},/*5*/{"",""},{"",""},{"",""},{"",""},{"",""},/*6*/{"",""},{"",""},{"",""},{"",""},{"",""},/*7*/};
+    String [][] IaboutCitiesAs = {{"",""},{"",""},{"",""},{"",""},{"",""},/*1*/{"",""},{"",""},{"",""},{"",""},{"",""},/*2*/
+        {"",""},{"",""},{"",""},{"",""},{"",""},/*3*/{"",""},{"",""},{"",""},{"",""},{"",""},/*4*/
+        {"",""},{"",""},{"",""},{"",""},{"",""},/*5*/{"",""},{"",""},{"",""},{"",""},{"",""},/*6*/{"",""},{"",""},{"",""},{"",""},{"",""},/*7*/};
+    String [][] IaboutCitiesEr = {{"",""},{"",""},{"",""},{"",""},{"",""},/*1*/{"",""},{"",""},{"",""},{"",""},{"",""},/*2*/
+        {"",""},{"",""},{"",""},{"",""},{"",""},/*3*/{"",""},{"",""},{"",""},{"",""},{"",""},/*4*/
+        {"",""},{"",""},{"",""},{"",""},{"",""},/*5*/{"",""},{"",""},{"",""},{"",""},{"",""},/*6*/{"",""},{"",""},{"",""},{"",""},{"",""},/*7*/};
+    //populoation=second area=first
     String [][] arrayS = new String[7][5];
 
     @Override
@@ -95,19 +107,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-
-    /*lass MedinotClass implements AdapterView.OnItemSelectedListener
-    {
-        public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-        ArrayAdapter<String> adpl = new ArrayAdapter<String>(this,
-                    R.layout.support_simple_spinner_dropdown_item, CitiesAfrica[0]);
-            lv.setAdapter(adpl);
-        }
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-        }
-    }
-     */
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
